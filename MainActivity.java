282cc6872737d3c389a06db9f316d54a4a4aab9e
karanleetcode <37148225+karanleetcode@users.noreply.github.com>
2018-10-27 02:18:47 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,88 @@
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    TextView addanote;
+    final DBHandler  dbHandler  = new DBHandler (this);      //database for main list
+    ArrayList<Contacts> arr = new ArrayList<>();
+    CustomAdapter customAdapter;        //fav list custom adapter
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        listView = findViewById(R.id.listview);
+        addanote = findViewById(R.id.addanote);
+        List<Contacts> contacts = dbHandler.getAllContacts();
+
+        for (Contacts cn : contacts) {
+            arr.add(cn);
+        }
+        if(arr.isEmpty() == true){addanote.setVisibility(View.VISIBLE);}else{addanote.setVisibility(View.INVISIBLE);}
+        customAdapter = new CustomAdapter(MainActivity.this, arr);
+        customAdapter.notifyDataSetChanged();
+        customAdapter.getObject(customAdapter);
+        listView.setAdapter(customAdapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(),Main2Activity.class);
+                intent.putExtra("title","0");
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+    }
+}
